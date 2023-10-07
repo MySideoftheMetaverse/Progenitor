5214f1d470d9e4e6ea3d5d82091d92a40687e7e3
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-07 17:44:58 -0500

    Update bang.sh

--- bang.sh
+++ bang.sh
@@ -3,10 +3,8 @@
 # Automatically shuts the script down when you encounter an error
 set -e
 
-
-
 #Install Brave
-sudo apt install curl
+sudo apt install curl dialog unzip xmlstarlet libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libx11-xcb-dev libpulse-dev libvulkan-dev libavcodec-dev libavformat-dev libavdevice-dev python3-pyudev telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
@@ -15,11 +13,9 @@ sudo apt install brave-browser
 # Install Rust
 curl https://sh.rustup.rs -s | sh 
 
-#Install desktop apps
-sudo apt install telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
-
 #Install h8mail
 git clone https://github.com/khast3x/h8mail.git
+cd h8mail
 sudo python3 setup.py install
 
 #Install Grub Customizer
@@ -33,7 +29,6 @@ cd addaura
 sudo ./addaura.sh
 
 #Install RetroPie
-sudo apt install dialog unzip xmlstarlet
 git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
 
 #Install WinApps
