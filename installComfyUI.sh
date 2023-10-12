40d8bb4b14142470e0c327e19b6b91cea5047222
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-12 13:16:17 -0500

    Create installComfyUI.sh

---
+++ installComfyUI.sh
@@ -0,0 +1,9 @@
+
+ #!/bin/bash
+ git clone https://github.com/comfyanonymous/ComfyUI.git && cd ComfyUI
+ 
+python -m pip install torch torchvision torchaudio –extra-index-url https://download.pytorch.org/whl/rocm5.4.2 -r requirements.txt
+
+get -c https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt -P ./models/checkpoints/
+
+python3 main.py --cpu --dont-upcast-attention --auto-launch --disable-cuda-malloc --disable-xformers 
