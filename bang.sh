11a4d9e618791fffe231fca4b05b619ced49c0e2
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-12 11:55:14 -0500

    Update bang.sh

--- bang.sh
+++ bang.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-
 # Automatically shuts the script down when you encounter an error
 set -e
 
+#Installs Dependencies
+sudo apt install curl libxcb-glx0-dev command-not-found apt-file plocate libfontconfig-dev apt-transport-https dialog unzip xmlstarlet libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libx11-xcb-dev libpulse-dev libvulkan-dev libavcodec-dev libavformat-dev libavdevice-dev python3-pyudev telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
+pip install psutil pyyaml
+sudo /etc/cron.daily/plocate
+
+#Installs Git LFS
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 
-sudo apt install command-not-found apt-file
-sudo apt install plocate && sudo /etc/cron.daily/plocate
-
-
+#Installs Go 
 wget https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.profile
 source $HOME/.profile
 
+#Installs Miniconda
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 
 #Install Brave
-sudo apt install curl libxcb-glx0-dev libfontconfig-dev apt-transport-https dialog unzip xmlstarlet libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libx11-xcb-dev libpulse-dev libvulkan-dev libavcodec-dev libavformat-dev libavdevice-dev python3-pyudev telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
+#Adds Flathub repo to Flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install Rust
@@ -56,9 +59,6 @@ git clone https://github.com/Fmstrat/winapps.git
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 sudo setfacl -m u:jellyfin:rx /media/sean.
 
-pip install psutil pyyaml
-
-
 
 #Install Notifiarr
 curl -s https://golift.io/repo.sh | sudo bash -s - notifiarr
@@ -72,5 +72,7 @@ sudo apt update
 sudo apt install ntfy
 sudo systemctl enable ntfy
 sudo systemctl start ntfy
+
+
 
 
