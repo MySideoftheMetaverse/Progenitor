d66f3098c1e8fb075ded59a5d693bdcc1d1da164
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-12 13:06:35 -0500

    Update installDependencies.sh

--- installDependencies.sh
+++ installDependencies.sh
@@ -2,6 +2,8 @@
 # Automatically shuts the script down when you encounter an error
 set -e
 
+sudo apt update
+
 #Installs Dependencies
 sudo apt install curl libxcb-glx0-dev command-not-found apt-file plocate libfontconfig-dev apt-transport-https dialog unzip xmlstarlet libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libx11-xcb-dev libpulse-dev libvulkan-dev libavcodec-dev libavformat-dev libavdevice-dev python3-pyudev telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
 pip install psutil pyyaml
