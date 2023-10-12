652c4ace5f3d62efd20bd3a90be9b4d5cc4d6e5a
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-12 12:24:35 -0500

    Create installMiniconda.sh

---
+++ installMiniconda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Automatically shuts the script down when you encounter an error
+set -e
+
+#Installs Miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
