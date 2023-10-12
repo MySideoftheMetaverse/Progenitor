604ce39d638e1e3bdef5bf7fc963843f0ddcf48c
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-12 08:29:56 -0500

    Update bang.sh
    git lfs

--- bang.sh
+++ bang.sh
@@ -3,6 +3,9 @@
 # Automatically shuts the script down when you encounter an error
 set -e
 
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+
 wget https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.profile
