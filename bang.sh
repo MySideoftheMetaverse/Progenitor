dbd8ab639ccd09f9fe2121c52d6a6eedf5c250c1
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-12 08:02:23 -0500

    Update bang.sh
    added go install

--- bang.sh
+++ bang.sh
@@ -3,6 +3,12 @@
 # Automatically shuts the script down when you encounter an error
 set -e
 
+wget https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.profile
+source $HOME/.profile
+
+
 #Install Brave
 sudo apt install curl libxcb-glx0-dev libfontconfig-dev apt-transport-https dialog unzip xmlstarlet libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libx11-xcb-dev libpulse-dev libvulkan-dev libavcodec-dev libavformat-dev libavdevice-dev python3-pyudev telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
