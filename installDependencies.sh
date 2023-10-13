7eb9e5c65f91d147691d4ef6607a65db61d13953
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-13 13:22:18 -0500

    Update installDependencies.sh

--- installDependencies.sh
+++ installDependencies.sh
@@ -6,15 +6,14 @@ sudo apt update
 
 #Installs Dependencies
 sudo apt install curl libxcb-glx0-dev command-not-found apt-file plocate libfontconfig-dev apt-transport-https dialog unzip xmlstarlet libudev-dev libxkbcommon-dev libsdl2-dev libasound2-dev libusb-1.0-0-dev libx11-xcb-dev libpulse-dev libvulkan-dev libavcodec-dev libavformat-dev libavdevice-dev python3-pyudev telegram-desktop git wget vlc snapd gramps python3-pip caffeine flatpak freerdp2-x11 virt-manager libclang-dev qbittorrent
-pip install psutil pyyaml
+pip3 install psutil pyyaml --break-system-packages
 sudo /etc/cron.daily/plocate
 
 #Installs Git LFS
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 
 
-#Installs Flatpak
- apt install flatpak
+#Installs Flatpak Repos
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 
