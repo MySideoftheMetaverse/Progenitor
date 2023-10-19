c37ef70d9302cb4c727d08245591b6c924358181
Blue <144849629+Furfulka@users.noreply.github.com>
2023-10-19 09:25:10 -0500

    Create installBedrock.sh

---
+++ installBedrock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.29/bedrock-linux-0.7.29-x86_64.sh
+sudo chmod +x bedrock-linux-0.7.29-x86_64.sh
+sudo sh bedrock-linux-0.7.29-x86_64.sh --hijack
